66ff69f09e5bb8c3b7e82fbea941b8314ce86fbb
sbasteiro <sbasteiro1@gmail.com>
2026-08-19 22:26:20 -0300

    Cambios boton

--- src/pages/StartScreen.tsx
+++ src/pages/StartScreen.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function StartScreen() {
     const navigate = useNavigate();
+
     const [blinking, setBlinking] = useState(true);
 
     useEffect(() => {
